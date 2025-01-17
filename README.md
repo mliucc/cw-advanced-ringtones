9d7a993327d69f239b039ab873cf8acef4d97571
mliucc <mliucc@163.com>
2025-01-17 20:56:28 +0800

    config.ini进行铃声配置（临时措施）

--- README.md
+++ README.md
@@ -6,11 +6,12 @@ cw-ring-personality，一个可以为Class Widgets提供**高度自定义铃声*
 - [x] 上课铃、下课铃、预备铃、午休铃、放学铃
 - [ ] 自定义其他铃声
 - [x] 通过修改main.py进行配置
+- [x] 通过修改config.json进行配置
 - [ ] 通过修改config.json进行配置
 - [ ] 通过设置UI进行配置
-## 如何使用（
+## 如何使用（暂时无法使用）
 - **安装插件**  在Class Widgets插件广场下载插件（暂未上架）
 - **更换音频**  打开Class Widgets主程序所在目录，转到./plugin/cw-ring_personality/plugin_audio，按照下面对应关系更换音频，要求为**wav格式** <br>
   >上课铃→attend_class.wav <br> 下课铃→finish_class.wav <br> 预备铃→prepare_class.wav <br> 午休铃→noon.wav <br> 放学铃→finish_school.wav
-- **配置铃声**  使用notepad或其他文本读写软件打开main.py，在#配置铃声区域#**两条同名注释**之间的区域**依照注释提示**修改配置
+- **配置铃声**  使用notepad或其他文本读写软件打开**Class Widgets主程序所在目录下的config.ini**，并将**插件目录下的config.ini**中的内容复制进**主程序的config.ini**，再**根据注释内容**修改配置文件进行配置
 - **启动插件**  打开Class Widgets设置页面，在**铃声页签**将程序自带通知铃声声音改至最小（**请务必确认该步骤完成，否则将同时响2个铃声**）；在**插件页签**将“铃声自定义”打开即可食用
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 #高级铃声插件v1.0.1
 
+#待实现功能：
+#config.json存储铃声配置
+#QtDesigner 设置UI
+#自定义其他铃声
+
 from PyQt5 import uic
 from loguru import logger
 from datetime import datetime
@@ -11,31 +16,36 @@ import os
 import conf
 from conf import base_directory
 
-#铃声文件名
-prepare_class = 'prepare_class.wav'
-attend_class = 'attend_class.wav'
-finish_class = 'finish_class.wav'
-noon = 'noon.wav'
-finish_school = 'finish_school.wav'
-
-#配置铃声区域
-noon_type = 0       #午餐铃或午自习铃对应通知类型（0为课间，1为上课）
-noon_class = '午自习'      #午餐铃或午自习铃对应课程(如上一项为1则在''内填写该活动名称；如上一项为0则在''内填写下面一个活动的活动名称，且上一节课、该课间与下一节课在同一节点内)
-vol = 75    #铃声音量，1～100的整数
-#配置铃声区域
 
 
 class Plugin(PluginBase):  # 插件类
     def __init__(self, cw_contexts, method):  # 初始化
         super().__init__(cw_contexts, method)  # 调用父类初始化方法
+        global playsound,prepare_class,attend_class,finish_class,noon,finish_school,default,noon_type,noon_class,vol
+
+        #铃声文件名(请在config.ini内配置)
+        prepare_class = conf.read_conf('File','prepare_class')
+        attend_class = conf.read_conf('File','attend_class')
+        finish_class = conf.read_conf('File','finish_class')
+        noon = conf.read_conf('File','noon')
+        finish_school = conf.read_conf('File','finish_school')
+        default = conf.read_conf('File','default')
+        print(attend_class)
+        #配置铃声区域(请在config.ini内配置)
+        noon_type = conf.read_conf('Noon','noon_type')       #午餐铃或午自习铃对应通知类型（见config.ini）
+        noon_class = conf.read_conf('Noon','noon_class')      #午餐铃或午自习铃对应课程(见config.ini)
+        vol = ('Volume','volume')    #铃声音量（见config.ini)
+        #配置铃声区域(请在config.ini内配置)
 
 
     def execute(self):  # 自启动执行部分
         global playsound
+        
         #播放铃声
         pygame.mixer.init()
         def playsound(filename):
             try:
+                print(base_directory)
                 file_path = os.path.join(base_directory, 'plugins', 'cw-ring-personality', 'plugin_audio', filename)
                 pygame.mixer.music.load(file_path)
                 volume = vol / 100
@@ -48,23 +58,44 @@ class Plugin(PluginBase):  # 插件类
         super().update(cw_contexts)  # 调用父类更新方法
         #判定主程序是否发送通知
         if self.method.is_get_notification():
-            if self.cw_contexts['Notification']['state'] == 2:    #判定是否放学
-                playsound(finish_school)
-                logger.info('插件cw-ring-personality播放铃声：放学')
-            elif self.cw_contexts['Notification']['state'] == noon_type and self.cw_contexts['Notification']['lesson_name'] == noon_class:    #判定是否午休
-                playsound(noon)
-                logger.info('插件cw-ring-personality播放铃声：午休')
-            else:
-                if self.cw_contexts['Notification']['state'] == 0:
-                    playsound(finish_class)
-                    logger.info('插件cw-ring-personality播放铃声：下课')
-                elif self.cw_contexts['Notification']['state'] == 1:
-                    playsound(attend_class)
-                    logger.info('插件cw-ring-personality播放铃声：上课')
-                elif self.cw_contexts['Notification']['state'] == 3:
-                    playsound(prepare_class)
-                    logger.info('插件cw-ring-personality播放铃声：准备上课')
+            if self.cw_contexts['Notification']['state'] == 2:    #判定放学
+                try:
+                    playsound(finish_school)
+                    logger.info('插件cw-ring-personality播放铃声：放学')
+                except Exception as e:
+                    logger.error(f'插件cw-ring-personality播放 放学 铃声出错：{e}')
+            elif self.cw_contexts['Notification']['state'] == noon_type and self.cw_contexts['Notification']['lesson_name'] == noon_class:    #判定午休
+                try:
+                    playsound(noon)
+                    logger.info('插件cw-ring-personality播放铃声：午休')
+                except Exception as e:
+                    logger.error(f'插件cw-ring-personality播放 午休 铃声出错：{e}')
+            else:    #判定普通铃声
+                if self.cw_contexts['Notification']['state'] == 0:    #判定下课
+                    try:
+                        playsound(finish_class)
+                        logger.info('插件cw-ring-personality播放铃声：下课')
+                    except Exception as e:
+                        logger.error(f'插件cw-ring-personality播放 下课 铃声出错：{e}')
+                elif self.cw_contexts['Notification']['state'] == 1:    #判定上课
+                    try:
+                        playsound(attend_class)
+                        logger.info('插件cw-ring-personality播放铃声：上课')
+                    except Exception as e:
+                        logger.error(f'插件cw-ring-personality播放 上课 铃声出错：{e}')
+                elif self.cw_contexts['Notification']['state'] == 3:    #判定预备
+                    try:
+                        playsound(prepare_class)
+                        logger.info('插件cw-ring-personality播放铃声：准备上课')
+                    except Exception as e:
+                        logger.error(f'插件cw-ring-personality播放 预备 铃声出错：{e}')
+                elif self.cw_contexts['Notification']['state'] == 4:    #判定其他通知
+                    try:
+                        playsound(default)
+                        logger.infof('插件cw-ring-personality检测到其他通知，将使用默认铃声')
+                    except Exception as e:
+                        logger.error(f'插件cw-ring-personality检测到 其他 通知，播放 默认 铃声出错：{e}')
                 else:
-                    logger.info('插件cw-ring-personality检测到其他通知，不进行打铃')
+                    logger.info('插件cw-ring-personality检测到未知通知，将不播放铃声，请检查主程序的日志以排查问题')
                     
 
